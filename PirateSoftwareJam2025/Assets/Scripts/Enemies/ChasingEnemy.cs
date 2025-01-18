aeb5ab16b82598d8f6fc8d2f52340e6fde31b90c
DungNC1 <142099972+DungNC1@users.noreply.github.com>
2025-01-18 19:33:41 +0700

    Basic Enemy Chasing

---
+++ PirateSoftwareJam2025/Assets/Scripts/Enemies/ChasingEnemy.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChasingEnemy : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    private Transform player;
+    private Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    private void Update()
+    {
+        if (player != null)
+        {
+            Vector3 direction = (player.position - transform.position).normalized;
+            rb.velocity = new Vector2(direction.x, direction.y) * speed;
+        }
+    }
+}
